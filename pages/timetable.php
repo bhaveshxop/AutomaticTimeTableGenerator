271d0c5b3cc4fb86c2fcac29e1d1486d0ff14661
bhaveshxop <113847606+bhaveshxop@users.noreply.github.com>
2024-02-28 00:16:59 +0530

    ok

--- pages/timetable.php
+++ pages/timetable.php
@@ -24,18 +24,58 @@ mysqli_close($conn);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Time Table</title>
+    <link rel="icon" href="../Images/logo.webp" type="image/x-icon">
+    <link rel="stylesheet" href="../css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../">
+                <img src="../Images/logo.webp" width="34" height="36">
+                TimeTable Generator</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="../">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./department.php">Departments</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="./class.php">Classes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./staff.php">Staff</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="./subjects.php">Subjects</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link " href="./aboutUs.php">About us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </nav>
+
+    <div class="d-grid gap-2 d-md-flex justify-content-md-start" style="margin-top: 35px; margin-left: 35px;">
+        <a href="javascript:history.back()" class="btn btn-primary">Back</a>
+    </div>
 
     <div class="container mt-3">
         <div class="head">
             <h2> Time Table </h2>
         </div>
 
-        <div class="table-responsive">
-                <table class="table table-bordered text-center table-hover table-responsive">
+        <div class="table-responsive ">
+                <table class="table table-bordered text-center table-hover table-responsive" style="border: 3px;">
                     <thead>
                         <tr>
                             <th>Day</th>
@@ -166,9 +206,9 @@ mysqli_close($conn);
                     </tbody>
                 </table>
                 <!-- Print button -->
-                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                    <button class="btn btn-success" onclick="window.print()">Print</button>
-                </div>
+                <div class="d-grid gap 2 d-md-flex justify-content-md-end">
+                    <button class="btn btn-primary" onclick="window.print()">Print</button>
+                    
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
