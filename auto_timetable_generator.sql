09074c8495807707f4496323d632095ca8c730d3
ameypte <ameypte@gmail.com>
2024-03-14 16:14:19 +0530

    Refactor code to improve performance and readability

--- auto_timetable_generator.sql
+++ auto_timetable_generator.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 27, 2024 at 04:21 PM
+-- Generation Time: Mar 14, 2024 at 11:33 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -61,25 +61,36 @@ CREATE TABLE `assigned` (
 --
 
 INSERT INTO `assigned` (`period_id`, `dept_id`, `year`, `section`, `sub_id`, `staff_short_name`, `duration`, `total_in_week`) VALUES
-(19, 2, 1, '1', 8, 'ASP', 1, 3),
-(20, 2, 1, '1', 8, 'ASP', 1, 3),
-(21, 2, 2, '1', 8, 'DAC', 2, 6),
-(22, 8, 2, '2', 10, 'DAC', 2, 5),
-(23, 8, 2, '2', 10, 'DAC', 2, 5),
-(24, 8, 1, '1', 8, 'ASP', 1, 3),
-(25, 32, 2, '2', 10, 'DAC', 2, 4),
-(26, 8, 1, '1', 8, 'ASP', 1, 3),
-(27, 8, 3, '1', 12, 'ASP', 2, 2),
-(28, 27, 2, '1', 15, 'ABD', 2, 3),
-(29, 8, 3, '1', 11, 'ASP', 1, 4),
-(30, 8, 3, '1', 16, 'DAC', 1, 4),
-(31, 8, 3, '2', 18, 'SNP', 1, 4),
-(32, 8, 3, '2', 17, 'HSB', 2, 2),
-(35, 27, 1, '1', 10, 'ABD', 1, 4),
-(38, 27, 1, '1', 18, 'BNP', 1, 6),
-(39, 27, 1, '1', 17, 'SNP', 2, 7),
-(40, 27, 1, '1', 14, 'DAC', 1, 4),
-(41, 27, 1, '1', 16, 'ASP', 1, 6);
+(63, 8, 1, '2', 41, 'SSD', 2, 2),
+(65, 8, 1, '2', 48, 'PSS', 2, 2),
+(67, 8, 1, '2', 51, 'MJG', 1, 4),
+(68, 8, 1, '2', 49, 'SPC', 2, 2),
+(69, 8, 1, '2', 42, 'PGB', 1, 3),
+(72, 8, 1, '2', 50, 'SPC', 1, 2),
+(73, 8, 1, '2', 45, 'SSB', 1, 4),
+(74, 8, 1, '2', 46, 'SSB', 2, 2),
+(76, 8, 1, '2', 47, 'PSS', 1, 2),
+(81, 8, 1, '2', 44, 'PGB', 1, 2),
+(83, 8, 2, '2', 37, 'SRC', 1, 5),
+(84, 8, 2, '2', 38, 'SRC', 2, 1),
+(85, 8, 2, '2', 40, 'PSS', 1, 2),
+(86, 8, 2, '2', 39, 'PSS', 2, 2),
+(87, 8, 2, '2', 31, 'KAS', 1, 4),
+(88, 8, 2, '2', 32, 'KAS', 2, 1),
+(89, 8, 2, '2', 35, 'PSS', 1, 3),
+(90, 8, 2, '2', 33, 'SNM', 1, 3),
+(91, 8, 2, '2', 36, 'PSS', 2, 1),
+(92, 8, 2, '2', 34, 'SNM', 2, 2),
+(93, 8, 3, '2', 20, 'SRC', 1, 3),
+(94, 8, 3, '2', 22, 'DTB', 1, 3),
+(95, 8, 3, '2', 21, 'DTB', 2, 1),
+(96, 8, 3, '2', 23, 'SPC', 1, 4),
+(97, 8, 3, '2', 24, 'SPC', 2, 2),
+(98, 8, 3, '2', 25, 'PSS', 1, 3),
+(99, 8, 3, '2', 26, 'KAS', 1, 3),
+(100, 8, 3, '2', 27, 'KAS', 2, 1),
+(101, 8, 3, '2', 28, 'NRP', 1, 2),
+(102, 8, 3, '2', 27, 'NRP', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -114,10 +125,35 @@ CREATE TABLE `departments` (
 --
 
 INSERT INTO `departments` (`id`, `dept_name`, `d_code`, `year`, `sections`) VALUES
-(2, 'Information technology', 'IF', 3, 1),
 (8, 'Computer Engineering', 'CM', 3, 2),
-(27, 'Electrical ', 'EE', 3, 1),
-(32, 'Mechanical Engineering ', 'ME', 2, 2);
+(32, 'Mechanical Engineering ', 'ME', 2, 2),
+(33, 'Civil Engineering ', 'CE', 3, 1),
+(36, 'Computer Science Engineering ', 'CW', 3, 2),
+(37, 'Electrical Engineering ', 'EE', 3, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rem_periods`
+--
+
+CREATE TABLE `rem_periods` (
+  `period_id` int(11) NOT NULL,
+  `dept_id` int(11) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `section` varchar(10) DEFAULT NULL,
+  `sub_id` int(11) DEFAULT NULL,
+  `staff_short_name` varchar(20) DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
+  `total_in_week` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rem_periods`
+--
+
+INSERT INTO `rem_periods` (`period_id`, `dept_id`, `year`, `section`, `sub_id`, `staff_short_name`, `duration`, `total_in_week`) VALUES
+(93, 8, 3, '2', 20, 'SRC', 1, 3);
 
 -- --------------------------------------------------------
 
@@ -135,12 +171,18 @@ CREATE TABLE `staff` (
 --
 
 INSERT INTO `staff` (`staff_name`, `short_name`) VALUES
-('Prof. Amit Barde', 'ABD'),
-('Prof. Amey S. Pathe', 'ASP'),
-('Dr. Bhavesh Patil', 'BNP'),
-('Dr. Durvesh A. Chaudhari', 'DAC'),
-('Prof. Heramb Bhoodhar', 'HSB'),
-('Prof. Sonu Patil', 'SNP');
+('Mr.D.T.Bharote', 'DTB'),
+('Mrs. J. M. Suryavanshi', 'JMS'),
+('Mrs.K.A.Sisodiya', 'KAS'),
+('Ms. M. J. Gujar', 'MJG'),
+('Mr.N.R.Patil', 'NRP'),
+('Mr. P. G. Bhadane', 'PGB'),
+('Mrs.P.S.Sonawane', 'PSS'),
+('Mrs.S.N.Madavi', 'SNM'),
+('Mr.S.P.Chavan', 'SPC'),
+('Ms.S.R.Chaudhari', 'SRC'),
+('Mr. S. S. Bafana', 'SSB'),
+('Mr. S. S. Dutte', 'SSD');
 
 -- --------------------------------------------------------
 
@@ -160,16 +202,39 @@ CREATE TABLE `subjects` (
 --
 
 INSERT INTO `subjects` (`sub_id`, `scode`, `sname`, `stype`) VALUES
-(8, 'FC0205', 'Programming in C', 'Lab'),
-(10, 'FC0205', 'Programming in C', 'Theory'),
-(11, 'FC5572', 'Java', 'Theory'),
-(12, 'FC7676', 'Android', 'Lab'),
-(13, 'FC5656', 'Php', 'Theory'),
-(14, 'FC5657', 'Php', 'Lab'),
-(15, 'FC8989', 'Signal and circuit', 'Lab'),
-(16, 'FC6565', 'Microprocessor', 'Theory'),
-(17, 'FC6565', 'Microprocessor', 'Lab'),
-(18, 'FC9898', 'Data scinece', 'Theory');
+(19, '22060', 'CPE', 'Lab'),
+(20, '22509', 'MGT', 'Theory'),
+(21, '22616', 'PWP', 'Lab'),
+(22, '22616', 'PWP', 'Theory'),
+(23, '22617', 'MAD', 'Theory'),
+(24, '22617', 'MAD', 'Lab'),
+(25, '22618', 'ETI', 'Theory'),
+(26, '22619', 'WBP', 'Theory'),
+(27, '22619', 'WBP', 'Lab'),
+(28, '22032', 'EDE', 'Theory'),
+(29, '22032', 'EDE', 'Lab'),
+(30, '-', 'Microproject', 'Lab'),
+(31, '22414', 'DCC', 'Theory'),
+(32, '22414', 'DCC', 'Lab'),
+(33, '22412', 'JPR', 'Theory'),
+(34, '22412', 'JPR', 'Lab'),
+(35, '22413', 'SEN', 'Theory'),
+(36, '22413', 'SEN', 'Lab'),
+(37, '22415', 'MIC', 'Theory'),
+(38, '22415', 'MIC', 'Lab'),
+(39, '22034', 'GAD', 'Lab'),
+(40, '22034', 'GAD', 'Theory'),
+(41, '312002', 'PCO', 'Lab'),
+(42, '312301', 'AMS', 'Theory'),
+(44, '312301', 'AMS', 'Tutorial'),
+(45, '312302', 'BEE', 'Theory'),
+(46, '312302', 'BEE', 'Lab'),
+(47, '22228', 'WPD', 'Theory'),
+(48, '22228', 'WPD', 'Lab'),
+(49, '3122001', 'BLP', 'Lab'),
+(50, '312001', 'BLP', 'Theory'),
+(51, '312303', 'PCI', 'Theory'),
+(52, '312303', 'PCI', 'Lab');
 
 -- --------------------------------------------------------
 
@@ -213,90 +278,93 @@ CREATE TABLE `time_table` (
 --
 
 INSERT INTO `time_table` (`id`, `day`, `period_id`, `start_time`, `end_time`, `recess`) VALUES
-(1, 'Monday', 19, '09:00:00', '10:00:00', 0),
-(2, 'Tuesday', 19, '09:00:00', '10:00:00', 0),
-(3, 'Wednesday', 19, '09:00:00', '10:00:00', 0),
-(4, 'Thursday', 20, '09:00:00', '10:00:00', 0),
-(5, 'Friday', 20, '09:00:00', '10:00:00', 0),
-(6, 'Saturday', 20, '09:00:00', '10:00:00', 0),
-(7, 'Monday', 21, '09:00:00', '11:00:00', 0),
-(8, 'Tuesday', 21, '09:00:00', '11:00:00', 0),
-(9, 'Wednesday', 21, '09:00:00', '11:00:00', 0),
-(10, 'Thursday', 21, '09:00:00', '11:00:00', 0),
-(11, 'Friday', 21, '09:00:00', '11:00:00', 0),
-(12, 'Saturday', 21, '09:00:00', '11:00:00', 0),
-(13, 'Monday', 22, '09:00:00', '11:00:00', 0),
-(14, 'Tuesday', 22, '09:00:00', '11:00:00', 0),
-(15, 'Wednesday', 22, '09:00:00', '11:00:00', 0),
-(16, 'Thursday', 22, '09:00:00', '11:00:00', 0),
-(17, 'Friday', 22, '09:00:00', '11:00:00', 0),
-(18, 'Saturday', 23, '09:00:00', '11:00:00', 0),
-(19, 'Monday', 23, '11:00:00', '13:00:00', 0),
-(20, 'Tuesday', 23, '11:00:00', '13:00:00', 0),
-(21, 'Wednesday', 23, '11:00:00', '13:00:00', 0),
-(22, 'Thursday', 23, '11:00:00', '13:00:00', 0),
-(23, 'Friday', 24, '09:00:00', '10:00:00', 0),
-(24, 'Saturday', 24, '09:00:00', '10:00:00', 0),
-(25, 'Monday', 24, '09:00:00', '10:00:00', 0),
-(26, 'Tuesday', 25, '09:00:00', '11:00:00', 0),
-(27, 'Wednesday', 25, '09:00:00', '11:00:00', 0),
-(28, 'Thursday', 25, '09:00:00', '11:00:00', 0),
-(29, 'Friday', 25, '09:00:00', '11:00:00', 0),
-(30, 'Saturday', 26, '10:00:00', '11:00:00', 0),
-(31, 'Monday', 26, '10:00:00', '11:00:00', 0),
-(32, 'Tuesday', 26, '09:00:00', '10:00:00', 0),
-(33, 'Wednesday', 27, '09:00:00', '11:00:00', 0),
-(34, 'Thursday', 27, '09:00:00', '11:00:00', 0),
-(35, 'Friday', 28, '09:00:00', '11:00:00', 0),
-(36, 'Saturday', 28, '09:00:00', '11:00:00', 0),
-(37, 'Monday', 28, '09:00:00', '11:00:00', 0),
-(38, 'Tuesday', 29, '09:00:00', '10:00:00', 0),
-(39, 'Wednesday', 29, '11:00:00', '12:00:00', 0),
-(40, 'Thursday', 29, '11:00:00', '12:00:00', 0),
-(41, 'Friday', 29, '09:00:00', '10:00:00', 0),
-(42, 'Saturday', 30, '09:00:00', '10:00:00', 0),
-(43, 'Monday', 30, '09:00:00', '10:00:00', 0),
-(44, 'Tuesday', 30, '10:00:00', '11:00:00', 0),
-(45, 'Wednesday', 30, '12:00:00', '13:00:00', 0),
-(46, 'Thursday', 31, '09:00:00', '10:00:00', 0),
-(47, 'Friday', 31, '09:00:00', '10:00:00', 0),
-(48, 'Saturday', 31, '09:00:00', '10:00:00', 0),
-(49, 'Monday', 31, '09:00:00', '10:00:00', 0),
-(50, 'Tuesday', 32, '09:00:00', '11:00:00', 0),
-(51, 'Wednesday', 32, '09:00:00', '11:00:00', 0),
-(52, 'Thursday', 35, '09:00:00', '10:00:00', 0),
-(53, 'Friday', 35, '09:00:00', '10:00:00', 0),
-(54, 'Saturday', 35, '09:00:00', '10:00:00', 0),
-(55, 'Monday', 35, '09:00:00', '10:00:00', 0),
-(56, 'Tuesday', 38, '09:00:00', '10:00:00', 0),
-(57, 'Wednesday', 38, '09:00:00', '10:00:00', 0),
-(58, 'Thursday', 38, '10:00:00', '11:00:00', 0),
-(59, 'Friday', 38, '10:00:00', '11:00:00', 0),
-(60, 'Saturday', 38, '10:00:00', '11:00:00', 0),
-(61, 'Monday', 38, '10:00:00', '11:00:00', 0),
-(62, 'Tuesday', 39, '10:00:00', '12:00:00', 0),
-(63, 'Wednesday', 39, '10:00:00', '12:00:00', 0),
-(64, 'Thursday', 39, '11:00:00', '13:00:00', 0),
-(65, 'Friday', 39, '11:00:00', '13:00:00', 0),
-(66, 'Saturday', 39, '11:00:00', '13:00:00', 0),
-(67, 'Monday', 39, '11:00:00', '13:00:00', 0),
-(68, 'Tuesday', 39, '12:00:00', '14:00:00', 0),
-(69, 'Wednesday', 40, '12:00:00', '13:00:00', 0),
-(70, 'Thursday', 40, '13:00:00', '14:00:00', 1),
-(71, 'Thursday', 40, '14:00:00', '15:00:00', 0),
-(72, 'Friday', 40, '13:00:00', '14:00:00', 1),
-(73, 'Friday', 40, '14:00:00', '15:00:00', 0),
-(74, 'Saturday', 40, '13:00:00', '14:00:00', 1),
-(75, 'Saturday', 40, '14:00:00', '15:00:00', 0),
-(76, 'Monday', 41, '13:00:00', '14:00:00', 1),
-(77, 'Monday', 41, '14:00:00', '15:00:00', 0),
-(78, 'Tuesday', 41, '14:00:00', '15:00:00', 1),
-(79, 'Tuesday', 41, '15:00:00', '16:00:00', 0),
-(80, 'Wednesday', 41, '13:00:00', '14:00:00', 1),
-(81, 'Wednesday', 41, '14:00:00', '15:00:00', 0),
-(82, 'Thursday', 41, '15:00:00', '16:00:00', 0),
-(83, 'Friday', 41, '15:00:00', '16:00:00', 0),
-(84, 'Saturday', 41, '15:00:00', '16:00:00', 0);
+(1, 'Monday', 63, '10:00:00', '12:00:00', 0),
+(2, 'Tuesday', 63, '10:00:00', '12:00:00', 0),
+(3, 'Wednesday', 65, '10:00:00', '12:00:00', 0),
+(4, 'Thursday', 65, '10:00:00', '12:00:00', 0),
+(5, 'Friday', 67, '10:00:00', '11:00:00', 0),
+(6, 'Saturday', 67, '10:00:00', '11:00:00', 0),
+(7, 'Monday', 67, '12:00:00', '13:00:00', 1),
+(8, 'Monday', 67, '13:00:00', '14:00:00', 0),
+(9, 'Tuesday', 67, '12:00:00', '13:00:00', 1),
+(10, 'Tuesday', 67, '13:00:00', '14:00:00', 0),
+(11, 'Wednesday', 68, '12:00:00', '13:00:00', 1),
+(12, 'Wednesday', 68, '13:00:00', '15:00:00', 0),
+(13, 'Thursday', 68, '12:00:00', '13:00:00', 1),
+(14, 'Thursday', 68, '13:00:00', '15:00:00', 0),
+(15, 'Friday', 69, '11:00:00', '12:00:00', 0),
+(16, 'Saturday', 69, '11:00:00', '12:00:00', 0),
+(17, 'Monday', 69, '14:00:00', '15:00:00', 0),
+(18, 'Tuesday', 72, '14:00:00', '15:00:00', 0),
+(19, 'Wednesday', 72, '15:00:00', '16:00:00', 0),
+(20, 'Thursday', 73, '15:00:00', '16:00:00', 0),
+(21, 'Friday', 73, '12:00:00', '13:00:00', 1),
+(22, 'Friday', 73, '13:00:00', '14:00:00', 0),
+(23, 'Saturday', 73, '12:00:00', '13:00:00', 1),
+(24, 'Saturday', 73, '13:00:00', '14:00:00', 0),
+(25, 'Monday', 73, '15:00:00', '16:00:00', 0),
+(26, 'Tuesday', 74, '15:00:00', '17:00:00', 0),
+(27, 'Friday', 74, '14:00:00', '16:00:00', 0),
+(28, 'Saturday', 76, '14:00:00', '15:00:00', 0),
+(29, 'Monday', 76, '16:00:00', '17:00:00', 0),
+(30, 'Wednesday', 81, '16:00:00', '17:00:00', 0),
+(31, 'Thursday', 81, '16:00:00', '17:00:00', 0),
+(32, 'Friday', 83, '10:00:00', '11:00:00', 0),
+(33, 'Saturday', 83, '10:00:00', '11:00:00', 0),
+(34, 'Monday', 83, '10:00:00', '11:00:00', 0),
+(35, 'Tuesday', 83, '10:00:00', '11:00:00', 0),
+(36, 'Wednesday', 83, '10:00:00', '11:00:00', 0),
+(37, 'Thursday', 84, '10:00:00', '12:00:00', 0),
+(38, 'Friday', 85, '11:00:00', '12:00:00', 0),
+(39, 'Saturday', 85, '11:00:00', '12:00:00', 0),
+(40, 'Monday', 86, '11:00:00', '13:00:00', 0),
+(41, 'Tuesday', 86, '11:00:00', '13:00:00', 0),
+(42, 'Wednesday', 87, '11:00:00', '12:00:00', 0),
+(43, 'Thursday', 87, '12:00:00', '13:00:00', 1),
+(44, 'Thursday', 87, '13:00:00', '14:00:00', 0),
+(45, 'Friday', 87, '12:00:00', '13:00:00', 1),
+(46, 'Friday', 87, '13:00:00', '14:00:00', 0),
+(47, 'Saturday', 87, '12:00:00', '13:00:00', 1),
+(48, 'Saturday', 87, '13:00:00', '14:00:00', 0),
+(49, 'Monday', 88, '13:00:00', '14:00:00', 1),
+(50, 'Monday', 88, '14:00:00', '16:00:00', 0),
+(51, 'Tuesday', 89, '13:00:00', '14:00:00', 1),
+(52, 'Tuesday', 89, '14:00:00', '15:00:00', 0),
+(53, 'Wednesday', 89, '12:00:00', '13:00:00', 1),
+(54, 'Wednesday', 89, '13:00:00', '14:00:00', 0),
+(55, 'Thursday', 89, '14:00:00', '15:00:00', 0),
+(56, 'Friday', 90, '14:00:00', '15:00:00', 0),
+(57, 'Saturday', 90, '14:00:00', '15:00:00', 0),
+(58, 'Monday', 90, '16:00:00', '17:00:00', 0),
+(59, 'Tuesday', 91, '15:00:00', '17:00:00', 0),
+(60, 'Wednesday', 92, '14:00:00', '16:00:00', 0),
+(61, 'Thursday', 92, '15:00:00', '17:00:00', 0),
+(62, 'Monday', 94, '10:00:00', '11:00:00', 0),
+(63, 'Tuesday', 94, '10:00:00', '11:00:00', 0),
+(64, 'Wednesday', 94, '10:00:00', '11:00:00', 0),
+(65, 'Thursday', 95, '10:00:00', '12:00:00', 0),
+(66, 'Friday', 96, '10:00:00', '11:00:00', 0),
+(67, 'Saturday', 96, '10:00:00', '11:00:00', 0),
+(68, 'Monday', 96, '11:00:00', '12:00:00', 0),
+(69, 'Tuesday', 96, '11:00:00', '12:00:00', 0),
+(70, 'Wednesday', 97, '11:00:00', '13:00:00', 0),
+(71, 'Thursday', 97, '12:00:00', '13:00:00', 1),
+(72, 'Friday', 97, '11:00:00', '13:00:00', 0),
+(73, 'Monday', 98, '12:00:00', '13:00:00', 1),
+(74, 'Monday', 98, '13:00:00', '14:00:00', 0),
+(75, 'Tuesday', 98, '12:00:00', '13:00:00', 1),
+(76, 'Wednesday', 98, '13:00:00', '14:00:00', 1),
+(77, 'Wednesday', 98, '14:00:00', '15:00:00', 0),
+(78, 'Thursday', 98, '13:00:00', '14:00:00', 0),
+(79, 'Friday', 99, '13:00:00', '14:00:00', 1),
+(80, 'Friday', 99, '14:00:00', '15:00:00', 0),
+(81, 'Saturday', 99, '11:00:00', '12:00:00', 0),
+(82, 'Tuesday', 99, '13:00:00', '14:00:00', 0),
+(83, 'Wednesday', 100, '15:00:00', '17:00:00', 0),
+(84, 'Thursday', 101, '14:00:00', '15:00:00', 0),
+(85, 'Friday', 101, '15:00:00', '16:00:00', 0),
+(86, 'Saturday', 102, '12:00:00', '13:00:00', 1),
+(87, 'Saturday', 102, '13:00:00', '15:00:00', 0);
 
 -- --------------------------------------------------------
 
@@ -316,7 +384,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(1, 'Amey Pathe', 'ameypte@gmail.com', 'admin');
+(1, 'Amey Pathe', 'ameypte@gmail.com', 'admin'),
+(2, 'sushma patil', 'sushma3@gmail.com', '12345');
 
 -- --------------------------------------------------------
 
@@ -356,6 +425,15 @@ ALTER TABLE `departments`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `rem_periods`
+--
+ALTER TABLE `rem_periods`
+  ADD PRIMARY KEY (`period_id`),
+  ADD KEY `fk_dept` (`dept_id`),
+  ADD KEY `fk_sub` (`sub_id`),
+  ADD KEY `fk_staff` (`staff_short_name`);
+
+--
 -- Indexes for table `staff`
 --
 ALTER TABLE `staff`
@@ -389,31 +467,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `assigned`
 --
 ALTER TABLE `assigned`
-  MODIFY `period_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `period_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 
 --
 -- AUTO_INCREMENT for table `departments`
 --
 ALTER TABLE `departments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT for table `rem_periods`
+--
+ALTER TABLE `rem_periods`
+  MODIFY `period_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
 
 --
 -- AUTO_INCREMENT for table `subjects`
 --
 ALTER TABLE `subjects`
-  MODIFY `sub_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `sub_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT for table `time_table`
 --
 ALTER TABLE `time_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
